14ffbb0e2dbf6dcf2b88cb7b4770aadf94e9bee3
UzziJordan <kpatakousman10@gmail.com>
2026-09-12 00:12:46 +0100

    api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+const getBaseUrl = () => {
+  const rawUrl = import.meta.env.VITE_API_BASE_URL;
+  if (!rawUrl) return "http://localhost:5000/api";
+
+  const cleanUrl = rawUrl.replace(/\/+$/, "");
+  return cleanUrl.endsWith("/api") ? cleanUrl : `${cleanUrl}/api`;
+};
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: getBaseUrl(),
   headers: {
     "Content-Type": "application/json",
   },
